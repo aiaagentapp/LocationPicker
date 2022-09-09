af85bb7584690d1100cc09d86eddd3cda9257999
aiaagentapp <49463797+aiaagentapp@users.noreply.github.com>
2022-09-09 17:18:14 +0800

    Update LocationPickerViewController.swift

--- LocationPicker/LocationPickerViewController.swift
+++ LocationPicker/LocationPickerViewController.swift
@@ -193,6 +193,11 @@ open class LocationPickerViewController: UIViewController {
 			getCurrentLocation()
 		}
 	}
+	
+	    open override func viewWillDisappear(_ animated: Bool) {
+        	// Resign first responder to avoid the search bar disappearing issue
+        	searchController.isActive = false
+    	}
 
 	open override var preferredStatusBarStyle : UIStatusBarStyle {
 		return statusBarStyle
@@ -386,7 +391,7 @@ extension LocationPickerViewController: MKMapViewDelegate {
 		if annotation is MKUserLocation { return nil }
 		
 		let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
-		pin.pinColor = .green
+		pin.pinTintColor = .green
 		// drop only on long press gesture
 		let fromLongPress = annotation is MKPointAnnotation
 		pin.animatesDrop = fromLongPress
@@ -427,7 +432,7 @@ extension LocationPickerViewController: MKMapViewDelegate {
 
 extension LocationPickerViewController: UIGestureRecognizerDelegate {
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
+        return false
     }
 }
 
